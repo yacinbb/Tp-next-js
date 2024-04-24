da6bb3907c0107d886863116506ce7a7ed1dabe8
Boudaya Yassin <boudayayacin007@gmail.com>
2024-04-24 15:01:59 +0100

    set up le menu

--- src/app/(admin)/admin/layout.js
+++ src/app/(admin)/admin/layout.js
@@ -1,5 +1,19 @@
 import SideBar from "@/components/admin/sideBar";
-export default function AdminLayout({ children }) {
+import { getServerSession } from 'next-auth'
+import { authOptions } from '../../api/auth/[...nextauth]/route'
+export default async function AdminLayout({ children }) {
+    const session = await getServerSession(authOptions)
+    if (!session || session && session.user.role !== 'admin') {
+    return (
+    <section className='py-24'>
+    <div className='container'>
+    <h1 className='text-2xl font-bold'>
+    You are not authorized to view this page
+    </h1>
+    </div>
+    </section>
+    )
+    }    
 return (
 <div className="row mt-4">
 <div className="col-md-3">
--- src/components/client/Menu.jsx
+++ src/components/client/Menu.jsx
@@ -7,16 +7,30 @@ import HomeIcon from '@mui/icons-material/Home';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import HelpIcon from '@mui/icons-material/Help';
 import 'bootstrap/dist/css/bootstrap.css';
+import { signIn, signOut, useSession } from 'next-auth/react';
+import { DynamicFeed, Logout } from '@mui/icons-material';
+import { NavDropdown } from 'react-bootstrap';
+import { Children } from 'react';
+
 function Menu() {
+  const {data: session} = useSession()
+  const title = <DynamicFeed />
 return (
   <>
       <Navbar bg="dark" data-bs-theme="dark" >
         <Container>
           <Navbar.Brand href="#home">BB Shop</Navbar.Brand>
           <Nav className="me-auto">
-          <Nav.Link as={Link} href="/"><HomeIcon/>Home</Nav.Link>    
-          <Nav.Link as={Link} href="/"><AccountCircleIcon/>Se connecter</Nav.Link>             
-          <Nav.Link as={Link} href="/client/pageAide"><HelpIcon />Aide</Nav.Link>          </Nav>
+            <NavDropdown title={title} >
+              {Children}
+            </NavDropdown>
+          <Nav.Link as={Link} href="/"><HomeIcon/>Home</Nav.Link> 
+          {
+            session? <Nav.Link onClick={()=>signOut()}><Logout/>Se déconnecter</Nav.Link>
+            : <Nav.Link onClick={()=> signIn()}><AccountCircleIcon/>Se connecter</Nav.Link>
+          }   
+          <Nav.Link as={Link} href="/client/pageAide"><HelpIcon />Aide</Nav.Link>   
+            </Nav>
         </Container>
       </Navbar>
     </>
